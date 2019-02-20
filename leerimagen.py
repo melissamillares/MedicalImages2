ef35fa7b9a2dd5b489c7a67ef4c9a2680fa3a058
melissamillares <diana.millares@correounivalle.edu.co>
2019-02-20 16:46:02 -0500

    histogram

--- leerimagen.py
+++ leerimagen.py
@@ -11,109 +11,17 @@ from matplotlib import pyplot as plt
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-aplicacion = tk.Tk()
-
-#variables to save info about the dcm file
-var = StringVar() 
-varModality = StringVar()
-varStudy = StringVar()
-varProtocol = StringVar()
-
-#function to choose the image 
-def openfile():   
-    archivo = tk.filedialog.askopenfile() #askdirectory()    
-    dcmfile = archivo.name   
-    print(dcmfile)         
-
-    # Get ref file
-    RefDs = pydicom.dcmread(dcmfile)#lstFilesDCM[0]
-    # Load dimensions based on the number of rows, columns, and slices (along the Z axis)
-    #ConstPixelDims = (int(RefDs.Rows), int(RefDs.Columns), len(lstFilesDCM))
-    ConstPixelDims = (int(RefDs.Rows), int(RefDs.Columns), 1)
-
-    # Load spacing values (in mm)
-    try:
-        SliceThickness = float(RefDs.SliceThickness)
-    except AttributeError:
-        SliceThickness = 1.0
-
-    ConstPixelSpacing = (float(RefDs.PixelSpacing[0]), float(RefDs.PixelSpacing[1]), SliceThickness)
-    #print(RefDs)
-    #print(ConstPixelDims)
-    #print(ConstPixelSpacing)
-    x = np.arange(0.0, (ConstPixelDims[0]+1)*ConstPixelSpacing[0], ConstPixelSpacing[0])
-    y = np.arange(0.0, (ConstPixelDims[1]+1)*ConstPixelSpacing[1], ConstPixelSpacing[1])
-    z = np.arange(0.0, (ConstPixelDims[2]+1)*ConstPixelSpacing[2], ConstPixelSpacing[2])
-
-    half = int(np.round(x.shape[0] / 2))
-    
-    # The array is sized based on 'ConstPixelDims'
-    ArrayDicom = np.zeros(ConstPixelDims, dtype=RefDs.pixel_array.dtype)
-
-    ds = pydicom.dcmread(dcmfile)
-    #print(ds)
-    # store the raw image data
-    ArrayDicom[:, :, 0] = ds.pixel_array
-    print(ds)
-    print(ds.pixel_array) 
-    print(ds.pixel_array.shape)# ds.pixel_array.shape returns the shape of a NumPy ndarray
-
-    setInfo(ds)
-    image(ds)
-    
-    img = np.flipud(ArrayDicom[:, :, 0])
-
-    #histogram    
-    hist,bins = np.histogram(img,256,[0,256])
-    plt.hist(img.ravel(),256,[0,256])
-    plt.title('Histogram')
-    plt.show()
-
-def image(file):      
-    plt.set_cmap(plt.gray()) 
-    imag = fig.add_subplot(111)    
-    imag.imshow(np.flipud(file.pixel_array))             
-    canvas.draw()    
-    canvas.get_tk_widget().pack()      
-    aplicacion.canvas.grid(row=2, column=1)
-
-def setInfo(header):
-    #variables with information about the dcm file
-    var.set(header.pixel_array.shape)  
-    modality = header.data_element("Modality")  
-    varModality.set(modality.value)
-    study = header.data_element("StudyDescription")  
-    varStudy.set(study.value)
-    protocol = header.data_element("ProtocolName")  
-    varProtocol.set(protocol.value)
-    #labels for the info
-    aplicacion.infolbl = tk.Label(master=back, text="Dimension (pixels):", font=('Ubuntu', 12), bg='#177497')
-    aplicacion.infolbl.grid(row=3, column=0, columnspan=2, padx=10, pady=10)
-    aplicacion.dimensionlbl = tk.Label(master=back, textvariable=var, font=('Ubuntu', 12), bg='#177497')
-    aplicacion.dimensionlbl.grid(row=4, column=0, columnspan=2, padx=10, pady=10, sticky=(N, S, E, W))
-
-    aplicacion.infomlbl = tk.Label(master=back, text="Modality:", font=("Ubuntu", 12), bg='#177497')
-    aplicacion.infomlbl.grid(row=5, column=0, columnspan=2, padx=10, pady=10)
-    aplicacion.modalitylbl = tk.Label(master=back, textvariable=varModality, font=("Ubuntu", 12), bg='#177497')
-    aplicacion.modalitylbl.grid(row=6, column=0, columnspan=2, padx=10, pady=10, sticky=(N, S, E, W))
-
-    aplicacion.infoslbl = tk.Label(master=back, text="Study description:", font=("Ubuntu", 12), bg='#177497')
-    aplicacion.infoslbl.grid(row=7, column=0, columnspan=2, padx=10, pady=10)
-    aplicacion.studylbl = tk.Label(master=back, textvariable=varStudy, font=("Ubuntu", 12), bg='#177497')
-    aplicacion.studylbl.grid(row=8, column=0, columnspan=2, padx=10, pady=10, sticky=(N, S, E, W))
-
-    aplicacion.infoplbl = tk.Label(master=back, text="Protocol Name:", font=("Ubuntu", 12), bg='#177497')
-    aplicacion.infoplbl.grid(row=9, column=0, columnspan=2, padx=10, pady=10)
-    aplicacion.protocollbl = tk.Label(master=back, textvariable=varProtocol, font=("Ubuntu", 12), bg='#177497')
-    aplicacion.protocollbl.grid(row=10, column=0, columnspan=2, padx=10, pady=10, sticky=(N, S, E, W))
-
-'''
-def convolution(imagen, kernel)
-    
-'''
-
 #Basics window interface
-#FRAME CONFIGURATION
+aplicacion = tk.Tk()
+aplicacion.geometry("600x750")
+text_frame = Frame(aplicacion)
+text_frame.configure(bg='#177497')
+text = tk.Text(text_frame, width = 90, height = 8)
+text.pack(side=LEFT, padx=20, pady=10)
+
+fig = Figure()
+canvas = FigureCanvasTkAgg(fig, master=aplicacion)
+
 title = 'pydicom image'
 back = tk.Frame(master=aplicacion,bg='#177497')
 back.master.title(title)
@@ -123,20 +31,103 @@ back.pack(fill=tk.BOTH, expand=1) #Expand the frame to fill the root window
 img = PhotoImage(file='./medical2.png')   
 #label title
 aplicacion.titulolbl = tk.Label(master=back, text="MEDICAL IMAGES", anchor="center", font=("Ubuntu", 28), bg='#177497')
-aplicacion.titulolbl.grid(row=0, column=0, columnspan=2, padx=10, pady=10, sticky=(N, S, E, W))
-#label for the histogram
-aplicacion.imagenlbl = tk.Label(master=back, image=img)
-aplicacion.imagenlbl.grid(row=1, column=0, columnspan=2, rowspan=2, padx=10, pady=10, sticky=(N, S, E, W))
+aplicacion.titulolbl.place(x=190, y=10)
+#label
+#aplicacion.imagenlbl = tk.Label(master=back, image=img)
+#aplicacion.imagenlbl.grid(row=1, column=0, columnspan=2, rowspan=2, padx=10, pady=10, sticky=(N, S, E, W))
+
+#function to choose the image 
+def openfile():   
+    archivo = tk.filedialog.askopenfile() #askdirectory()    
+    dcmfile = archivo.name   
+    print(dcmfile)         
+    # Get ref file
+    RefDs = pydicom.dcmread(dcmfile)#lstFilesDCM[0]
+    # Load dimensions based on the number of rows, columns, and slices (along the Z axis)
+    #ConstPixelDims = (int(RefDs.Rows), int(RefDs.Columns), len(lstFilesDCM))
+    ConstPixelDims = (int(RefDs.Rows), int(RefDs.Columns), 1)
+    # The array is sized based on 'ConstPixelDims'
+    ArrayDicom = np.zeros(ConstPixelDims, dtype=RefDs.pixel_array.dtype)
+    ds = pydicom.dcmread(dcmfile)
+
+    # store the raw image data
+    ArrayDicom[:, :, 0] = ds.pixel_array
+    #print(ds)
+    print(ds.pixel_array) 
+    print(ds.pixel_array.shape)# ds.pixel_array.shape returns the shape of a NumPy ndarray
+    
+    imageProcessing(ds)
+    histogram(ds)
+    
+    img = np.flipud(ArrayDicom[:, :, 0])
+
+    '''#histogram    
+    hist,bins = np.histogram(img,256,[0,256])
+    plt.hist(img.ravel(),256,[0,256])
+    plt.title('Histogram')
+    plt.show()'''
+
+def setInfo(header):
+    text.delete('1.0', END)
+    text.config(state=NORMAL) 
+    #variables with information about the dcm file     
+    acquisition = header.data_element("MRAcquisitionType")
+    modality = header.data_element("Modality")  
+    study = header.data_element("StudyDescription")      
+    protocol = header.data_element("ProtocolName") 
+    patients_sex = header.data_element("PatientSex")
+    patients_age = header.data_element("PatientAge")
+    info = "*Image Dimension: " + str(header.pixel_array.shape)
+    info += "\n*MR Acquisition Type: " + acquisition.value
+    info += "\n*Modality: " + modality.value 
+    info += "\n*Study Description: " + study.value
+    info += "\n*Protocol Name: " + protocol.value    
+    info += "\n*Patient's Sex: " + patients_sex.value
+    info += "\n*Patient's Age: " + patients_age.value
+    text.delete('1.0', END)
+    text.insert('1.0', info)
+    text.config(state=DISABLED)
+    
+def histogram(file):    
+    rows = int(file.Rows)
+    columns = int(file.Columns)
+    pixelArray = file.pixel_array    
+    array = [0]*65536
+    
+    for i in range(rows):
+        for j in range(columns):
+            array[pixelArray[i,j]]=array[pixelArray[i,j]]+1
+            
+    array = np.asarray(array)
+    plt.plot(array)
+    fig = plt.gcf()
+    fig.canvas.set_window_title('Histogram')           
+    plt.show()  
+
+def imageProcessing(file):     
+    text_frame.pack()  
+    setInfo(file)    
+    plt.set_cmap(plt.gray()) 
+    imag = fig.add_subplot(111)    
+    imag.imshow(np.flipud(file.pixel_array))             
+    canvas.draw()        
+              
+
+'''
+def convolution(imagen, kernel):
+    
+'''
 #button to open the image
 aplicacion.abrirbtn = tk.Button(master=back, text="Open image", command=openfile)
 aplicacion.abrirbtn.pack(side="top")
-aplicacion.abrirbtn.grid(row=11, column=0, pady=5)
+aplicacion.abrirbtn.place(x=50, y=70)
+#cbb 
+
 #button to close the app
 aplicacion.quit = tk.Button(master=back, text="Exit", fg="red", command=aplicacion.destroy)
 aplicacion.quit.pack(side="top")
-aplicacion.quit.grid(row=11, column=1, pady=5)
+aplicacion.quit.place(x=500, y=70)
 
-fig = Figure()
-canvas = FigureCanvasTkAgg(fig, master=aplicacion)
+canvas.get_tk_widget().pack()
 
 aplicacion.mainloop()
